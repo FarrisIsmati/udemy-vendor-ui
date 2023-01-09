8a2b3e0c29d6d290be52023697514d73aa7ecbe2
farris.ismati@gmail.com <farris.ismati@gmail.com>
2023-01-09 10:10:18 -0500

    updated config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
     GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
     VENDORS_API_URL: process.env.VENDORS_API_URL,
     VENDORS_WEBSOCKET_URL: process.env.VENDORS_WEBSOCKET_URL
-  }
+  },
+  output: 'standalone'
 }
 
 module.exports = nextConfig
