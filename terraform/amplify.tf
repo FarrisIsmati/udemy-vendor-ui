a80226622ebf5c0268c45fa4cce4bc652caf6742
farris.ismati@gmail.com <farris.ismati@gmail.com>
2023-01-11 13:47:39 -0500

    remove comment

--- terraform/amplify.tf
+++ terraform/amplify.tf
@@ -1,4 +1,3 @@
-# - env | grep -e NEXT_PUBLIC_ >> .env.production will add all NEXT_PUBLIC_* in prod to env var 
 resource "aws_amplify_app" "frontend" {
   name = "${var.app_name}"
   repository = "https://github.com/FarrisIsmati/udemy-vendor-ui"
